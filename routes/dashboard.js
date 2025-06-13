cd94a9d993e3dac758010a69182c33e3dea92b8a
HemantDahiyaCodes <hemantdahiyacodes@gmail.com>
2025-06-13 22:11:12 +0530

    Added the passport.authenticate Added authentication on this route that allowed to have access by url path because of missing authentication

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const dashboardRoute = express();
+const passport = require("passport");
+
 
 dashboardRoute.get("/", (req, res) => {
     res.render("dashboard");
-});
+})
+
+
+dashboardRoute.post("/", passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/log-in",
+}));
 
 module.exports = dashboardRoute;
